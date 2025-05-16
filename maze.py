6adfade0ae4aa8094d6b9f26d1415b8ece60392f
postscript91 <piotr@slomczykowski.pl>
2025-05-16 18:38:27 +0200

    final touches

--- maze.py
+++ maze.py
@@ -121,7 +121,7 @@ class Maze:
         return self._solve_r(0, 0)
 
     def _solve_r(self, i, j):
-        self._animate
+        self._animate()
         
         self._cells[i][j].visited = True
         
